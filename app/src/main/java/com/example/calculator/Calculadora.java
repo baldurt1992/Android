0d1b290e47a3c6c38f9e6bb5b1333b3b686acbf1
baldurt1992 <andresbaldur92@gmail.com>
2024-05-02 13:41:52 -0500

    calculadora

---
+++ app/src/main/java/com/example/calculator/Calculadora.java
@@ -0,0 +1,42 @@
+package com.example.calculator;
+
+public class Calculadora {
+    public float sumar (float[] numeros) {
+        float sumarTotal = 0;
+        for (float numero : numeros) {
+            sumarTotal += numero;
+        }
+        return sumarTotal;
+    }
+
+    public float restar (float[] numeros) {
+        float restarTotal = numeros[0];
+        for (int i = 1; i < numeros.length; i++) {
+            restarTotal -= numeros[i];
+        }
+        return restarTotal;
+
+    }
+
+
+    public float multiplicar (float[] numeros) {
+        float multiTotal = 1;
+        for (float numero : numeros) {
+            multiTotal *= numero;
+        }
+        return multiTotal;
+    }
+
+    public float dividir (float[] numeros) {
+        float divTotal = numeros[0];
+        for (int i = 1; i < numeros.length; i++) {
+            if (numeros[i] != 0) {
+                divTotal /= numeros[i];
+            } else {
+                throw new ArithmeticException("División por cero");
+            }
+        }
+        return divTotal;
+    }
+
+}
---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.text.NumberFormat;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText value1, value2;
+    TextView result;
+    Button sum, res, multi, div;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        sum = findViewById(R.id.sum);
+        res = findViewById(R.id.res);
+        multi = findViewById(R.id.multi);
+        div = findViewById(R.id.div);
+        value1 = findViewById(R.id.valor1);
+        result = findViewById(R.id.result);
+
+        sum.setOnClickListener(v -> operaciones("1"));
+
+        res.setOnClickListener(v -> operaciones("2"));
+
+        multi.setOnClickListener(v -> operaciones("3"));
+
+        div.setOnClickListener(v -> operaciones("4"));
+
+        }
+    private void operaciones(String operador) {
+        String usuarioInput = value1.getText().toString();
+        String[] valorStr = usuarioInput.split(" ");
+
+        float[] numeros = new float[valorStr.length];
+
+        for(int i = 0; i < valorStr.length; i++ ) {
+            numeros[i] = Float.parseFloat(valorStr[i]);
+        }
+        Calculadora calculadora = new Calculadora();
+        float resultado = 0;
+
+        switch (operador) {
+            case "1":
+                resultado = calculadora.sumar(numeros);
+                break;
+            case "2":
+                resultado = calculadora.restar(numeros);
+                break;
+            case "3":
+                resultado = calculadora.multiplicar(numeros);
+                break;
+            case "4":
+                if (numeros.length > 1) {
+                    resultado = calculadora.dividir(numeros);
+                } else {
+                    result.setText("Error: Se requieren al menos dos números para la división");
+                    return;
+                }
+                break;
+        }
+        NumberFormat resultadoExacto = NumberFormat.getInstance();
+        String resultadoFormateado = resultadoExacto.format(resultado);
+        if (operador.equals("4") && resultado % 1 != 0) {
+            result.setText(String.valueOf(resultado));
+        } else {
+            result.setText(resultadoFormateado);
+        }
+
+    }
+
+}
+
+
